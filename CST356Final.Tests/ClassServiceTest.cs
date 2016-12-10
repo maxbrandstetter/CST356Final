d5d77668d5a9e7337f127173a19340cb6b3f68df
Max Brandstetter <middlestetter999@gmail.com>
2016-12-09 22:07:11 -0800

    New unit test for classes

---
+++ CST356Final.Tests/ClassServiceTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CST356Final.Services;
+using CST356Final.Models;
+
+namespace CST356Final.Tests
+{
+    [TestFixture]
+    public class ClassServiceTest
+    {
+        private IClassService _classService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _classService = new ClassService();
+        }
+
+        [Test]
+        public void WhenAlphabeticServiceFails()
+        {
+            var c = new Class
+            {
+                Subject = "Writing",
+                ClassNumber = "Oops",
+                ClassName = "Intro to Writing"
+            };
+
+            var result = _classService.ClassNumberIsNumeric(c);
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void WhenNumericServiceSucceeds()
+        {
+            var c = new Class
+            {
+                Subject = "Writing",
+                ClassNumber = "101",
+                ClassName = "Intro to Writing"
+            };
+
+            var result = _classService.ClassNumberIsNumeric(c);
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void WhenAlphaNumericServiceFails()
+        {
+            var c = new Class
+            {
+                Subject = "Writing",
+                ClassNumber = "101aba101",
+                ClassName = "Intro to Writing"
+            };
+
+            var result = _classService.ClassNumberIsNumeric(c);
+
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ CST356Final/Services/ClassService.cs
@@ -0,0 +1,23 @@
+﻿using CST356Final.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace CST356Final.Services
+{
+    public class ClassService : IClassService
+    {
+        public bool ClassNumberIsNumeric(Class c)
+        {
+            // Check if class number is all numbers
+            if (Regex.IsMatch(c.ClassNumber, @"^\d+$"))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
